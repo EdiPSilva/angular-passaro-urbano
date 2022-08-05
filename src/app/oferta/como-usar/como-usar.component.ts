bc0bd7bc66e377b69d857efea46b5b4b504a3192
edisilva <edipaulinodasilva@gmail.com>
2022-08-04 21:01:07 -0300

    construindo componentes internos

---
+++ src/app/oferta/como-usar/como-usar.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OfertasService } from 'src/app/ofertas.service';
+import { ComoUsar } from 'src/app/shared/como-usar.model';
+
+@Component({
+  selector: 'app-como-usar',
+  templateUrl: './como-usar.component.html',
+  styleUrls: ['./como-usar.component.css'],
+  providers: [OfertasService]
+})
+export class ComoUsarComponent implements OnInit {
+
+  public comoUsar: string = "";
+
+  constructor(private route: ActivatedRoute, private ofertasService: OfertasService) { }
+
+  ngOnInit(): void {
+    let id = this.route.parent?.snapshot.params['id'];
+
+    this.ofertasService.getComoUsarOfertaPorId(id)
+      .then((comoUsar: ComoUsar) => {
+        this.comoUsar = comoUsar.descricao;
+      })
+      .catch((params: any) => {
+        console.log(params);
+      });
+  }
+}
